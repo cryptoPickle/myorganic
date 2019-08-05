4d1a759f4e1f323d3ecb61e76b2b03e2398edf66
Halil <cryptopickle@outlook.com>
2019-08-05 22:51:40 +0100

    scripts

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,14 +1,43 @@
 #!/bin/sh
+set -e
+ARG=$1
 
-cd ../src
+if [ "$#" -eq 0 ] ; then
+  echo "install_packages.sh <install | reinstall | clean>"
+  exit 1
+fi
 
-for l in $(find . -mindepth 2 -maxdepth 2 -type d); do
-  cd $l
+if [ ! -d "node_modules" ] ; then
+  echo "Installing Serverless Libraries";
+  npm install
+fi
 
-  if [ -a "package.json" ]; then
-    echo "Installing libraries $l..."
-    npm install
-  fi
-done
+cd src
 
-echo "Done!"
+if [ $ARG == "install" ] || [ $ARG ==  "reinstall" ] ; then
+  for l in $(find . -mindepth 2 -maxdepth 2 -type d); do
+    cd $l
+
+    if [ -a "package.json" ]; then
+      echo "Installing libraries $l..."
+      if [ $ARG == "reinstall" ] ; then
+        echo "Removing node_modules"
+        rm -rf node_modules
+      fi
+      npm install
+    fi
+  done
+  cd - > /dev/null
+  echo "Done!"
+fi
+
+if [ $ARG == "clean" ] ; then
+  for l in $(find . -mindepth 2 -maxdepth 2 -type d); do
+    cd $l
+    if [ -d "node_modules" ] ; then
+      echo "Cleaning $l"
+      rm -rf node_modules
+    fi
+  done
+    cd - > /dev/null
+fi
